31bc971a6923eef33def7f006c5f5045a337f846
barreirobots <barreirobots@gmail.com>
2021-11-18 10:17:51 -0500

    Added refactored auto file NewAuto.java Added an encoder based auto file NewPPRAuto.java Added a new Teleop NewTeleop.java Added a webcam test file TestWebcam.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewPPRAuto.java
@@ -0,0 +1,401 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+
+@Autonomous(name="HashIsABadSpelerAuto")
+public class NewPPRAuto extends LinearOpMode {
+    private DcMotor FLMotor;
+    private DcMotor FRMotor;
+    private DcMotor BLMotor;
+    private DcMotor BRMotor;
+    private DcMotor Flywheel;
+    private final int PPR = 145;
+    private final double DrivePower = 0.75;
+    // private DcMotor HorizontalSlidePack;
+    // private DcMotor VerticalSlidePack;
+    // private DcMotor EaterMotor;
+    // private Blinker expansion_Hub_4;
+    // private Orientation lastAngles = new Orientation();
+    // double globalAngle, power = .30, correction;
+    private ElapsedTime eTime = new ElapsedTime();
+
+    enum DriveDirection {
+        FORWARD,
+        LEFT,
+        RIGHT
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        //Maps hardware for all parts
+        FLMotor = hardwareMap.dcMotor.get("0");
+        FRMotor = hardwareMap.dcMotor.get("1");
+        BLMotor = hardwareMap.dcMotor.get("2");
+        BRMotor = hardwareMap.dcMotor.get("3");
+        Flywheel = hardwareMap.dcMotor.get("Fly");
+        // HorizontalSlidePack = hardwareMap.dcMotor.get("HorizontalSlidePack");
+        // VerticalSlidePack = hardwareMap.dcMotor.get("VerticalSlidePack");
+        // EaterMotor = hardwareMap.dcMotor.get("Eater");
+
+        FRMotor.setDirection(DcMotor.Direction.FORWARD);
+        FLMotor.setDirection(DcMotor.Direction.REVERSE);
+        BRMotor.setDirection(DcMotor.Direction.FORWARD);
+        BLMotor.setDirection(DcMotor.Direction.FORWARD);
+        Flywheel.setDirection(DcMotor.Direction.FORWARD);
+        // HorizontalSlidePack.setDirection(DcMotor.Direction.FORWARD);
+        // VerticalSlidePack.setDirection(DcMotor.Direction.FORWARD);
+        // EaterMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        telemetry.addData("Status","TeleOp");
+        telemetry.update();
+
+        //Configures settings for different parts
+        FLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BLMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BRMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Flywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // HorizontalSlidePack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // VerticalSlidePack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // EaterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        FLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Flywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // HorizontalSlidePack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // VerticalSlidePack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        // EaterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        FLMotor.setPower(DrivePower);
+        BRMotor.setPower(DrivePower);
+        FRMotor.setPower(DrivePower);
+        BLMotor.setPower(DrivePower);
+
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.update();
+
+        // wait for start button.
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        sleep(50);
+        //The actual program
+        eTime.reset();
+        drive(DriveDirection.FORWARD, DrivePower, 1 * PPR);
+        sleep(500); // For Testing Purposes
+        drive(DriveDirection.LEFT, DrivePower, 1 * PPR);
+        sleep(500);
+        drive(DriveDirection.FORWARD, DrivePower, 5 * PPR);
+    }
+
+    private void drive(DriveDirection direction, double power, int position) {
+        eTime.reset();
+        switch(direction) {
+            case FORWARD:
+                FLMotor.setTargetPosition(position);
+                FRMotor.setTargetPosition(position);
+                BLMotor.setTargetPosition(position);
+                BRMotor.setTargetPosition(position);
+                break;
+            case LEFT:
+                FLMotor.setTargetPosition(-1 * position);
+                FRMotor.setTargetPosition(position);
+                BLMotor.setTargetPosition(-1 * position);
+                BRMotor.setTargetPosition(position);
+                break;
+            case RIGHT:
+                FLMotor.setTargetPosition(position);
+                FRMotor.setTargetPosition(-1 * position);
+                BLMotor.setTargetPosition(position);
+                BRMotor.setTargetPosition(-1 * position);
+                break;
+        }
+//         while(opModeIsActive() && eTime.milliseconds() < time){
+//             telemetry.addData("Time:", eTime);
+//             telemetry.update();
+//         }
+//         FLMotor.setPower(0);
+//         FRMotor.setPower(0);
+//         BLMotor.setPower(0);
+//         BRMotor.setPower(0);
+    }
+    private void timedrive(double power, double time){
+        eTime.reset();
+        while(opModeIsActive() && eTime.milliseconds() < time){
+            FLMotor.setPower(power);
+            FRMotor.setPower(power);
+            BLMotor.setPower(power);
+            BRMotor.setPower(power);
+            telemetry.addData("Time:", eTime);
+            telemetry.update();
+        }
+        FLMotor.setPower(0);
+        FRMotor.setPower(0);
+        BLMotor.setPower(0);
+        BRMotor.setPower(0);
+    }
+    // private void strafe(double power, double time){
+    //     eTime.reset();
+    //     while(opModeIsActive() && eTime.seconds() < time){
+    //         FLMotor.setPower(-power);
+    //         FRMotor.setPower(-power);
+    //         BLMotor.setPower(-power);
+    //         BRMotor.setPower(-power);
+    //         telemetry.addData("Time:", eTime);
+    //         telemetry.update();
+    //     }
+    //     FLMotor.setPower(0);
+    //     FRMotor.setPower(0);
+    //     BLMotor.setPower(0);
+    //     BRMotor.setPower(0);
+    // }
+    private void leftturn(double power, double time){
+        eTime.reset();
+        while(opModeIsActive() && eTime.milliseconds() < time){
+            FLMotor.setPower(-1 * power);
+            FRMotor.setPower(1 * power);
+            BLMotor.setPower(-1 * power);
+            BRMotor.setPower(1 * power);
+        }
+        FLMotor.setPower(0);
+        FRMotor.setPower(0);
+        BLMotor.setPower(0);
+        BRMotor.setPower(0);
+    }
+    private void rightturn(double power, double time){
+        eTime.reset();
+        while(opModeIsActive() && eTime.milliseconds() < time){
+            FLMotor.setPower(-1 * power);
+            FRMotor.setPower(1 * power);
+            BLMotor.setPower(-1 * power);
+            BRMotor.setPower(1 *power);
+        }
+        FLMotor.setPower(0);
+        FRMotor.setPower(0);
+        BLMotor.setPower(0);
+        BRMotor.setPower(0);
+    }
+
+
+
+
+    /*(gamepad2.a){
+            LGServo.setPosition(0);
+            RGServo.setPosition(0.75);
+        }else if (gamepad2.b){
+            LGServo.setPosition(0.75);
+            RGServo.setPosition(0);
+        }
+    */
+//    private void resetAngle()
+//    {
+//        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+//
+//        globalAngle = 0;
+//    }
+//
+//    //Get Angle Function
+//    private double getAngle()
+//    {
+//        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+//
+//        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+//
+//        if (deltaAngle < -180)
+//            deltaAngle += 360;
+//        else if (deltaAngle > 180)
+//            deltaAngle -= 360;
+//
+//        globalAngle += deltaAngle;
+//
+//        lastAngles = angles;
+//
+//        return globalAngle;
+//    }
+
+    //Check Direction Function
+//    private double checkDirection()
+//    {
+//        double correction, angle, gain = .10;
+//
+//        angle = getAngle();
+//
+//        if (angle == 0)
+//            correction = 0;
+//        else
+//            correction = -angle;
+//
+//        correction = correction * gain;
+//
+//        return correction;
+//    }
+//
+//    //Rotate Function
+//    private void rotate(double power, int degrees)
+//    {
+//        if(opModeIsActive())
+//        {
+//            double currentAng = 0;
+//            resetAngle();
+//            if (power > 0)
+//            {
+//                while(opModeIsActive() && (-currentAng) < degrees)
+//                {
+//                    currentAng  =   getAngle();
+//                    FLMotor.setPower(power);
+//                    FRMotor.setPower(-power);
+//                    BLMotor.setPower(power);
+//                    BRMotor.setPower(-power);
+//
+//                    telemetry.addData("Angle: ", currentAng);
+//                    telemetry.addData("Target: ", degrees);
+//                }
+//            }
+//
+//            else
+//            {
+//                degrees = -degrees;
+//                while(opModeIsActive() && (currentAng) < degrees)
+//                {
+//                    currentAng  =   getAngle();
+//                    FLMotor.setPower(power);
+//                    FRMotor.setPower(-power);
+//                    BLMotor.setPower(power);
+//                    BRMotor.setPower(-power);
+//
+//                    telemetry.addData("Angle: ", currentAng);
+//                    telemetry.addData("Target: ", degrees);
+//                }
+//            }
+//
+//            FLMotor.setPower(0);
+//            FRMotor.setPower(0);
+//            BLMotor.setPower(0);
+//            BRMotor.setPower(0);
+//
+//        }
+//    }
+
+    //Encoder Drive Function
+    // public void drive(double power, int centimeters)
+    // {
+
+    //     int currentPosFR    = 0;
+    //     int currentPosFL    = 0;
+    //     int currentPosBR    = 0;
+    //     int currentPosBL    = 0;
+    //     int target = centimeters * 45;
+
+    //     // Ensure that the opmode is still active
+    //     if (opModeIsActive())
+    //     {
+    //         FRMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+    //         FLMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+    //         BLMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+    //         BRMotor.setMode(DcMotor.RunMode.RESET_ENCODERS);
+
+    //         // Turn On RUN_TO_POSITION
+    //         FLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    //         BRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    //         FRMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    //         BLMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    //         // Determine new target position, and pass to motor controller
+    //         BRMotor.setTargetPosition(target);
+    //         FLMotor.setTargetPosition(target);
+    //         BLMotor.setTargetPosition(target);
+    //         FRMotor.setTargetPosition(target);
+
+    //         FLMotor.setPower(power);
+    //         BRMotor.setPower(power);
+    //         FRMotor.setPower(power);
+    //         BLMotor.setPower(power);
+
+    //         // keep looping while we are still active, and there is time left, and both motors are running.
+    //         // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+    //         // its target position, the motion will stop.  This is "safer" in the event that the robot will
+    //         // always end the motion as soon as possible.
+    //         // However, if you require that BOTH motors have finished their moves before the robot continues
+    //         // onto the next step, use (isBusy() || isBusy()) in the loop test.
+    //         if (power > 0)
+    //         {
+    //             while (opModeIsActive() && (currentPosFL < target || currentPosBR < target ||
+    //             currentPosFR < target || currentPosBL < target))
+    //             {
+    //                 currentPosBR    = BRMotor.getCurrentPosition();
+    //                 currentPosFL    = FLMotor.getCurrentPosition();
+    //                 currentPosFR    = FRMotor.getCurrentPosition();
+    //                 currentPosBL    = BLMotor.getCurrentPosition();
+
+    //                 // Display it for the driver.
+    //                 telemetry.addData("Running to ", target);
+    //                 telemetry.addData("Running at front left: ",  currentPosFL);
+    //                 telemetry.addData("Running at back right: ", currentPosBR);
+    //                 telemetry.addData("Running at front right: ",  currentPosFR);
+    //                 telemetry.addData("Running at back left: ", currentPosBL);
+    //                 telemetry.update();
+    //             }
+    //         }
+
+    //         else
+    //         {
+    //             while (opModeIsActive() && (currentPosFL > target || currentPosBR > target ||
+    //             currentPosFR > target || currentPosBL > target))
+    //             {
+    //                 currentPosBR    = BRMotor.getCurrentPosition();
+    //                 currentPosFL    = FLMotor.getCurrentPosition();
+    //                 currentPosFR    = FRMotor.getCurrentPosition();
+    //                 currentPosBL    = BLMotor.getCurrentPosition();
+
+    //                 // Display it for the driver.
+    //                 telemetry.addData("Running to ", target);
+    //                 telemetry.addData("Running at front left: ",  currentPosFL);
+    //                 telemetry.addData("Running at back right: ", currentPosBR);
+    //                 telemetry.addData("Running at front right: ",  currentPosFR);
+    //                 telemetry.addData("Running at back left: ", currentPosBL);
+    //                 telemetry.update();
+    //             }
+    //         }
+    //     }
+
+//        // Stop all motion;
+//        BRMotor.setPower(0);
+//        FRMotor.setPower(0);
+//        FLMotor.setPower(0);
+//        BLMotor.setPower(0);
+//
+//        // Turn off RUN_TO_POSITION
+//        FLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        BRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        FRMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        BLMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    //  sleep(250);   // optional pause after each move
+
+
+//Land Function
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewTeleop.java
@@ -0,0 +1,178 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+
+@TeleOp(name="HashIsABadSpelerTeleop", group="Linear Opmode")
+//@Disabled
+public class NewTeleop extends LinearOpMode {
+
+    private DcMotor FLMotor;
+    private DcMotor FRMotor;
+    private DcMotor BLMotor;
+    private DcMotor BRMotor;
+    private DcMotor Flywheel;
+    private CRServo Flapper;
+    private double drive;
+    private double turn;
+    private final double DriveSpeed = 0.75;
+    private final double FlapperDownPosition = 0.3;
+
+    @Override
+    public void runOpMode () {
+
+        FLMotor = hardwareMap.dcMotor.get("0");
+        FRMotor = hardwareMap.dcMotor.get("1");
+        BLMotor = hardwareMap.dcMotor.get("2");
+        BRMotor = hardwareMap.dcMotor.get("3");
+        Flywheel = hardwareMap.dcMotor.get("Fly");
+        Flapper = hardwareMap.crservo.get("Flap");
+//        DcMotor HorizontalSlidePack = hardwareMap.dcMotor.get("HorizontalSlidePack");
+//        DcMotor VerticalSlidePack = hardwareMap.dcMotor.get("VerticalSlidePack");
+//        DcMotor EaterMotor = hardwareMap.dcMotor.get("Eater");
+
+        FRMotor.setDirection(DcMotor.Direction.REVERSE);
+        FLMotor.setDirection(DcMotor.Direction.FORWARD);
+        BRMotor.setDirection(DcMotor.Direction.REVERSE);
+        BLMotor.setDirection(DcMotor.Direction.REVERSE);
+        Flywheel.setDirection(DcMotor.Direction.FORWARD);
+        Flapper.setDirection(DcMotorSimple.Direction.FORWARD);
+//        HorizontalSlidePack.setDirection(DcMotor.Direction.FORWARD);
+//        VerticalSlidePack.setDirection(DcMotor.Direction.FORWARD);
+//        EaterMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+        telemetry.addData("Status","TeleOp");
+        telemetry.update();
+
+
+        while(opModeIsActive()) {
+
+            //Driver 1
+//            drive = -gamepad1.left_stick_y;
+//            turn = gamepad1.right_stick_x;
+            double LeftDrive = gamepad1.left_stick_y * DriveSpeed;
+            double RightDrive = gamepad1.right_stick_y * DriveSpeed;
+//            double LeftDrive = Range.clip(drive + turn, -1.0, 1.0) * DriveSpeed;
+//            double RightDrive = Range.clip(drive - turn, -1.0, 1.0) * DriveSpeed;
+            double FlywheelClockwise = gamepad1.left_bumper ? -1 : 0;
+            double FlywheelCounterClockwise = gamepad1.right_bumper ? 1 : 0;
+            double LeftStrafe = gamepad1.left_trigger;
+            double RightStrafe = gamepad1.right_trigger;
+            boolean FlapperUp = gamepad2.y;
+            boolean FlapperDown = gamepad2.a;
+//            double HorizontalSlidePackForward = gamepad2.right_bumper ? 1 : 0;
+//            double HorizontalSlidePackBackward = gamepad2.right_trigger > 0 ? 1 : gamepad2.right_trigger < 0 ? -1 : 0;
+//            double VerticalSlidePackForward = gamepad2.dpad_up ? 1 : 0;
+//            double VerticalSlidePackBackward = gamepad2.dpad_down ? 1 : 0;
+//            double EaterForward = gamepad2.circle ? 1 : 0;
+//            double EaterBackward = gamepad2.cross ? 1 : 0;
+
+            if (FlywheelClockwise != 0 || FlywheelCounterClockwise != 0) {
+                Flywheel.setPower(0.5 * (FlywheelCounterClockwise + FlywheelClockwise));
+            } else {
+                Flywheel.setPower(0);
+            }
+
+            if (FlapperUp || FlapperDown) {
+                Flapper.getController().setServoPosition(Flapper.getPortNumber(), FlapperUp ? 0 : FlapperDownPosition);
+            }
+
+//            if (HorizontalSlidePackBackward != 0 || HorizontalSlidePackForward != 0) {
+//                HorizontalSlidePack.setPower(- HorizontalSlidePackBackward + HorizontalSlidePackForward);
+//            }
+//
+//            if (VerticalSlidePackBackward != 0 || VerticalSlidePackForward != 0) {
+//                VerticalSlidePack.setPower(- VerticalSlidePackBackward + VerticalSlidePackForward);
+//            }
+//
+//            if (EaterBackward != 0 || EaterForward != 0) {
+//                EaterMotor.setPower(- EaterBackward + EaterForward);
+//            }
+
+//            double LeftDrive = DriveSpeed * (gamepad1.left_stick_y);
+//            double RightDrive = DriveSpeed * (gamepad1.right_stick_y);
+            // if (LeftStrafe == 0 && RightStrafe == 0) {
+            // FLMotor.setPower(LeftDrive);
+            // BLMotor.setPower(LeftDrive);
+            // FRMotor.setPower(RightDrive);
+            // BRMotor.setPower(RightDrive);
+            // } else if (LeftStrafe > 0) {
+            //     FLMotor.setPower(-1);
+            //     BLMotor.setPower(1);
+            //     FRMotor.setPower(1);
+            //     BRMotor.setPower(-1);
+            // } else if (RightStrafe > 0) {
+            //     FLMotor.setPower(1);
+            //     BLMotor.setPower(-1);
+            //     FRMotor.setPower(-1);
+            //     BRMotor.setPower(1);
+            // }
+            // if (LeftDrive != 0 && RightDrive != 0) {
+            FLMotor.setPower(LeftDrive);
+            BLMotor.setPower(LeftDrive);
+            FRMotor.setPower(RightDrive);
+            BRMotor.setPower(RightDrive);
+            // }
+            // else if (LeftDrive > 0) {
+            //     FLMotor.setPower(LeftDrive);
+            //     BLMotor.setPower(LeftDrive);
+            //     FRMotor.setPower(LeftDrive * 0.5);
+            //     BRMotor.setPower(LeftDrive * 0.5);
+            // } else if (RightDrive > 0) {
+            //     FLMotor.setPower(RightDrive * 0.5);
+            //     BLMotor.setPower(RightDrive * 0.5);
+            //     FRMotor.setPower(RightDrive);
+            //     BRMotor.setPower(RightDrive);
+            // } else {
+            //     FLMotor.setPower(0 * 0.5);
+            //     BLMotor.setPower(0 * 0.5);
+            //     FRMotor.setPower(0);
+            //     BRMotor.setPower(0);
+            // }
+            telemetry.addData("LeftDrive", LeftDrive);
+            telemetry.addData("RightDrive", RightDrive);
+            telemetry.addData("Flywheel", FlywheelCounterClockwise + FlywheelClockwise);
+//            telemetry.addData("HorizontalSlidePack", -HorizontalSlidePackBackward + HorizontalSlidePackForward);
+//            telemetry.addData("VerticalSlidePack", -VerticalSlidePackBackward + VerticalSlidePackForward);
+//            telemetry.addData("Eater", -EaterBackward + EaterForward);
+
+            telemetry.update();
+        }
+    }
+}
